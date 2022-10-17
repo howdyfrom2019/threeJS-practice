f63aa99a05274199f19827d744416e8b0768b214
Jaake <49380742+howdyfrom2019@users.noreply.github.com>
2022-10-17 19:54:00 +0900

    1 sec after playing

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 let audioResources = document.getElementsByClassName("audio-file");
-audioResources[0].volume = 0.1;
-audioResources[0].play();
+audioResources[0].volume = 0.5;
+setTimeout(() => {
+  audioResources[0].play();
+}, 1000);
