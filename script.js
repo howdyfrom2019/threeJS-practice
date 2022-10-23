53af420c10b2a4f0532af755f862d2aee31cbc58
howdyfrom2019 <tjd1emd@naver.com>
2022-10-23 23:56:08 +0900

    mobile and other songs.

--- script.js
+++ script.js
@@ -2,10 +2,26 @@ const audioResources = document.getElementsByClassName("audio-file");
 const playButtons = document.getElementsByClassName("play");
 const pauseButtons = document.getElementsByClassName("pause");
 const progressTracker = document.getElementById("line");
+const sections = document.getElementsByClassName("album");
+const navigation = document.getElementsByClassName("circle");
+let currPage = 0;
+const totalPage = sections.length;
+
+const allMusicStop = (index) => {
+  for (let i = 0; i < audioResources.length; i++) {
+    const audio = audioResources[i];
+    if (audio.readyState < 2) return;
+    if (i !== index) {
+      audio.pause();
+      audio.currentTime = 0;
+    }
+  }
+}
 
 const toggleMusic = (index) => {
   if (audioResources[index].readyState < 2) return;
-  audioResources[index].volume = 0.4;
+  allMusicStop(index);
+  audioResources[index].volume = 0.5;
   if (audioResources[index].duration > 0 && !audioResources[index].paused) {
     audioResources[index].pause();
     pauseButtons[index].classList.replace("visible", "invisible");
@@ -23,6 +39,21 @@ const fillProgress = (e) => {
   progressTracker.style.width = Math.floor((currentTime / duration) * 100) + "%";
 }
 
-playButtons[0].addEventListener("click", () => toggleMusic(0));
-pauseButtons[0].addEventListener("click", () => toggleMusic(0));
-audioResources[0].addEventListener("timeupdate", fillProgress);
+const nextPage = (index) => {
+  console.log(navigation);
+  for (let i = 0; i < navigation.length; i++) {
+    const el = navigation[i];
+    el.classList.remove("active");
+    sections[i].classList.remove("active");
+  }
+  navigation[index].classList.add("active");
+  sections[index].classList.add("active");
+  currPage = index;
+}
+
+for (let i = 0; i < playButtons.length; i++) {
+  playButtons[i].addEventListener("click", () => toggleMusic(i));
+  pauseButtons[i].addEventListener("click", () => toggleMusic(i));
+  audioResources[i].addEventListener("timeupdate", fillProgress);
+  navigation[i].addEventListener("click", () => nextPage(i));
+}
