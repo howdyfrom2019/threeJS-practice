a25d8cf0170429f5aff3c71aa4142cbdefc47a7b
howdyfrom2019 <tjd1emd@naver.com>
2022-10-24 00:28:43 +0900

    change background and text.

--- script.js
+++ script.js
@@ -4,6 +4,26 @@ const pauseButtons = document.getElementsByClassName("pause");
 const progressTracker = document.getElementById("line");
 const sections = document.getElementsByClassName("album");
 const navigation = document.getElementsByClassName("circle");
+const fullPage = document.getElementsByClassName("full-page");
+const title = document.getElementsByClassName("song-title")[0];
+const singer = document.getElementsByClassName("singer-3")[0];
+const highlight = document.getElementsByClassName("song-highlight")[0];
+const diskInner = document.getElementsByClassName("disk_inner");
+
+const gradientColor = [
+  ["#DF3371", "#25ADDC"],
+  ["#13a113", "#00ff00"],
+  ["#b10000", "#d59b79"],
+  ["#6b3c28", "#a57f5d"],
+  ["#cc4b2c", "#ed4c26"],
+];
+const text = [
+  { title: "Gradation", singer: "10cm", highLight: "달콤한 색감이 물들어 조금씩 <br />정신 차렸을 땐 알아볼 수도 없지" },
+  { title: "After Like", singer: "IVE", highLight: "아마 꿈만 같겠지만 분명 꿈이 아니야<br/>달리 설명할 수 없는, 이건 사랑일 거야" },
+  { title: "Nxde", singer: "(G)I-DLE", highLight: "쉿, yes, I'm a nxde.<br />Yes, I'm a nxde" },
+  { title: "STAR WALKIN'", singer: "Lill Nas X", highLight: "Don't ever say it's over<br />if I'm breathing" },
+  { title: "I want to", singer: "MINO", highLight: "조명 빛이 넘 밝아도<br />타이밍이 안 좋아도 걍 하고 싶어!" },
+]
 let currPage = 0;
 const totalPage = sections.length;
 
@@ -39,8 +59,24 @@ const fillProgress = (e) => {
   progressTracker.style.width = Math.floor((currentTime / duration) * 100) + "%";
 }
 
+const changeBackgroundColor = () => {
+  const background = fullPage[0];
+  background.style.background = "linear-gradient(305.74deg,"+ gradientColor[currPage][0] +" 10.26%, "+ gradientColor[currPage][1] + " 92.21%)";
+}
+
+const changeText = () => {
+  const { title: currTitle, singer: currSinger, highLight: currHighLight } = text[currPage];
+  title.innerText = currTitle;
+  singer.innerText = currSinger;
+  console.log(text[currPage]);
+  highlight.innerHTML = currHighLight;
+}
+
+const changeDiskColor = () => {
+  diskInner[currPage].style.backgroundColor = gradientColor[currPage][0];
+}
+
 const nextPage = (index) => {
-  console.log(navigation);
   for (let i = 0; i < navigation.length; i++) {
     const el = navigation[i];
     el.classList.remove("active");
@@ -49,6 +85,9 @@ const nextPage = (index) => {
   navigation[index].classList.add("active");
   sections[index].classList.add("active");
   currPage = index;
+  changeBackgroundColor();
+  changeText();
+  changeDiskColor();
 }
 
 for (let i = 0; i < playButtons.length; i++) {
